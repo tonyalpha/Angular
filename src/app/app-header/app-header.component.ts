ca1d077db11b18c8e3d99567ae223ac9abc1e5c8
Anton Sherri <anton.sherri@vdabheverlee.intra>
2019-11-04 10:07:29 +0100

    inital setup, only header

---
+++ src/app/app-header/app-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'media-app-header',
+  templateUrl: './app-header.component.html',
+  styleUrls: ['./app-header.component.css']
+})
+export class AppHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
